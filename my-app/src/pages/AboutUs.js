fe2e0420443a450fbc61f0d1ff0dcb67b89c5cce
pbudiman <61896774+pbudiman@users.noreply.github.com>
2020-09-04 15:21:18 +1000

    added instructions on about us page

--- my-app/src/pages/AboutUs.js
+++ my-app/src/pages/AboutUs.js
@@ -1,5 +1,25 @@
 import React from 'react';
 
+import './aboutUs.css';
+
 export default function AboutUs(){
-    
+    return(
+       
+        <div className='about-container'>
+            <div className='whatwedo'>
+                <h1>What we do.</h1>
+                <h2>Explain what the web app does here.</h2>
+            </div>
+            <div className='instructions'>
+                <h2>How to Use.</h2>
+            </div>
+            <div className='meettheteam'>
+                <h2>Meet the team.</h2>
+            </div>
+        
+            
+            
+        </div>
+         
+    );
 }
