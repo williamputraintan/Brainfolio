2c8c555700d66acf69e606878adee10788b4583f
William <Wintan@student.unimelb.edu.au>
2020-09-29 23:17:24 +1000

    remove GetHellp

--- brainfolio-api/src/app.module.ts
+++ brainfolio-api/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProjectsModule} from './projects/projects.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
@@ -9,7 +7,5 @@ require('dotenv').config()
 
 @Module({
   imports: [ProjectsModule, MongooseModule.forRoot(process.env.MONGO_URL)],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
