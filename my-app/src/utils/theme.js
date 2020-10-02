a7fc6cca81563f7cabb29842fe5703ebbe43e806
franklinaldo <franklin.aldo@yahoo.com>
2020-10-02 10:58:35 +1000

    Responsive porfolio page

--- my-app/src/utils/theme.js
+++ my-app/src/utils/theme.js
@@ -7,11 +7,7 @@ const theme = createMuiTheme({
       background: 'linear-gradient(to right, #da22ff, #9733ee)'
     },
     primary: { 
-<<<<<<< HEAD
       main: '#1D3B64'
-=======
-      main: '#004AAF'
->>>>>>> project-backend
     },
     secondary: {
       main: '#A0CFE7',
