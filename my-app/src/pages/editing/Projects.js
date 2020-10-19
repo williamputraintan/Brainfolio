a7bf678f05f684120b88e1983c8c9484d70611a9
William <Wintan@student.unimelb.edu.au>
2020-10-19 19:48:48 +1100

    update

--- my-app/src/pages/editing/Projects.js
+++ my-app/src/pages/editing/Projects.js
@@ -38,17 +38,6 @@ export default function Projects() {
     const [allProjects, setAllProjects] =  React.useState([]);
     const [filesToUpload, setFilesToUpload] = React.useState([])
     const [filesToDelete, setFilesToDelete] = React.useState([])
-
-    useEffect(() => {
-
-      axios.get("http://localhost:5000/projects/")
-      .then((response) => {
-        const responseData = response.data;
-        setAllProjects(responseData);
-
-      })
-    },[fields]);
-
     // fields form
     const [fields, setFields] = React.useState({
       _id:"",
@@ -60,14 +49,20 @@ export default function Projects() {
       contributor:[],
       projectFileName:[[]],
     })
-  
-    // file
-    const [filesToDisplay, setFilesToDisplay] =  React.useState([]);
+
+
+    useEffect(() => {
+
+      axios.get("http://localhost:5000/projects/")
+      .then((response) => {
+        const responseData = response.data;
+        setAllProjects(responseData);
+
+      })
+    },[fields]);
 
 
     function onFormInputChange(e){
-
-
       setFields({
         ...fields,
         [e.target.name]: e.target.value
@@ -76,40 +71,15 @@ export default function Projects() {
       console.log('delte = ', filesToDelete);
       console.log('allproj = ', allProjects);
       console.log('fields = ', fields);
-      console.log('filesToDisplay = ',filesToDisplay);
     }
 
 
     function onFileChangeUpload(e){
-      setFilesToUpload(e.target.files)
-      // setFields({...fields, files: e.target.files})
-      // // setFilesToUpload(e.target.files);
-      // const filesToUpload = e.target.files
-
-      // console.log('savefiles');
-      // const formData = new FormData();
-      // for(let eachFile of filesToUpload){
-      //   formData.append('files', eachFile)
-      // }
-      // formData.append('_id', fields._id)
-      // console.log(formData.getAll('files'));
-      // axios.post("http://localhost:5000/projects/files", formData)
-      // .then((response) => {
-        
-      //   const project_id= response.data._id;
-      //   setFields({...fields , _id : project_id})
-
-      //   setFilesToDisplay(filesToDisplay.concat(response.data.projectFileName))
-      //   document.getElementById('inputFile').value = ''
-      // })
-      // .catch(err => {
-      //   console.log(err);
-      // })
-     
+      setFilesToUpload(e.target.files)    
     }
+
     function onDeleteFile(e, fileName){
       e.preventDefault();
-
       setFilesToDelete(filesToDelete.concat(fileName))
     }
   
@@ -129,7 +99,6 @@ export default function Projects() {
         formData.append('filesToUpload', eachFile)
       }
 
-
       for ( var key in fields ) {
         formData.append(key, fields[key]);
       }
@@ -140,9 +109,7 @@ export default function Projects() {
       formData.append('filesToDelete', '')
       formData.append('filesToDelete', '')
       console.log('DELTE = ', formData.get('filesToDelete'));
-      // for(let eachFile of filesToDelete){
-      //   formData.append('filesToDelete', eachFile)
-      // }
+
       axios.post("http://localhost:5000/projects/save/", formData)
       .then((response) => {
         console.log(response);
@@ -169,6 +136,8 @@ export default function Projects() {
     const handleClose = () => {
       setOpen(false);
     };
+
+    
     const[oneName,setOneName] = React.useState("");
     const[oneEmail,setOneEmail]= React.useState("");
 
