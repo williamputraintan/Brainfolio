b8d96979babb7976ffc6e75ee2a0d1dcb8adf4b5
pbudiman <61896774+pbudiman@users.noreply.github.com>
2020-10-25 07:25:01 +1100

    edit double type info

--- my-app/src/pages/editing/DoubleTypeInfo.js
+++ my-app/src/pages/editing/DoubleTypeInfo.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
@@ -9,41 +8,15 @@ import MuiDialogActions from '@material-ui/core/DialogActions';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import Typography from '@material-ui/core/Typography';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab'
-import Box from '@material-ui/core/Box';
-import PropTypes from 'prop-types';
 import ListItem from '@material-ui/core/ListItem';
 import Divider from '@material-ui/core/Divider';
-
 import EditButton from './EditButton.js'
-
-const styles = (theme) => ({
-  root: {
-    margin: 0,
-    paddingTop: theme.spacing(2),
-    paddingBottom: theme.spacing(2),
-    minWidth: '300px',
-
-  },
-  closeButton: {
-    position: 'absolute',
-    right: theme.spacing(1),
-    top: theme.spacing(1),
-    color: theme.palette.grey[500],
-  } ,
-  
-  container:{
-    overflowY:"scroll",
-    maxHeight:"700px",
-  },
-  tabRoot: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
-  },
-
-});
+import PropTypes from 'prop-types';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Box from '@material-ui/core/Box';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -52,8 +25,8 @@ function TabPanel(props) {
     <div
       role="tabpanel"
       hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
       {...other}
     >
       {value === index && (
@@ -73,11 +46,34 @@ TabPanel.propTypes = {
 
 function a11yProps(index) {
   return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
+    id: `full-width-tab-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`,
   };
 }
 
+const styles = (theme) => ({
+  tabRoot: {
+    backgroundColor: "#FFFFFF",
+    flexGrow: 1
+  },
+  root: {
+    margin: 0,
+    padding: theme.spacing(2),
+    minWidth: '300px',
+
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  } ,
+  
+  container:{
+    overflowY:"scroll",
+    maxHeight:"600px"
+  }
+});
 
 const DialogTitle = withStyles(styles)((props) => {
   const { children, classes, onClose, ...other } = props;
@@ -120,7 +116,7 @@ export default function DoubleTypeInfo(props) {
 
   //used in Divider usage
   var count = 0;
-    
+  const theme = useTheme();
   const [open, setOpen] = React.useState(false);
   const [value, setValue] = React.useState(0);
 
@@ -132,10 +128,10 @@ export default function DoubleTypeInfo(props) {
     setOpen(false);
   };
 
-  const handleChange = (newValue) => {
+  const handleTabChange = (event, newValue) => {
     setValue(newValue);
   };
-  
+
   //disregard unwanted props
   function checkUnwanted(key, value){
     return (key!=="_id" && key!=="username" && key!=="__v" && key!=="onGoing" && value!=="");
@@ -152,19 +148,16 @@ export default function DoubleTypeInfo(props) {
       setOpen(false);
   }
 
-  function handleDate(date){
-    var formatDate = date.substring(0,10);
-    return formatDate;
-  }   
-  function handleValue(key,value,res){
-      if(key==="startDate"){
-          return handleDate(value);
-      } else if(key==="endDate" && res.hasOwnProperty('onGoing') && !res.onGoing){
-          return handleDate(value);
-      } else{
-          return value;
-      }
-  }
+  function handleDataValue(key,value,res){
+    if(key==="startDate"){
+        return value.substring(3,15);
+    } else if(key==="endDate" && res.hasOwnProperty('onGoing') && !res.onGoing){
+        return value.substring(3,15);
+    } else{
+        return value;
+    }
+}
+
 
   return (
     <div>
@@ -176,42 +169,46 @@ export default function DoubleTypeInfo(props) {
           Your {title}
         </DialogTitle>
         <DialogContent dividers className={styles.container}>
-          <Typography gutterBottom>
-            <div className={styles.tabRoot}>
-              <AppBar position="static" color="#000">
-                <Tabs value={value} indicatorColor="primary" onChange={handleChange} >
-                  <Tab label={tab1} {...a11yProps(0)} />
-                  <Tab label={tab2} {...a11yProps(1)} />
-                </Tabs>
-              </AppBar>
-              <TabPanel value={value} index={0}>
-                  {tab1List.map(res=>(
-                  <div>
-                    {/* endDate value is onGoing when ongoing is checked */}
-                    <div style={{display:'none'}}>{res.hasOwnProperty('onGoing') && res.onGoing?res.endDate="On Going" :null}</div>
+        <div className={styles.tabRoot}>
+          <AppBar position="static" color="default">
+            <Tabs
+              value={value}
+              onChange={handleTabChange}
+              indicatorColor="primary"
+              textColor="primary"
+              variant="fullWidth"
+              aria-label="full width tabs example"
+            >
+              <Tab label={tab1} {...a11yProps(0)} />
+              <Tab label={tab2} {...a11yProps(1)} />
+            </Tabs>
+          </AppBar>
+            <TabPanel value={value} index={0} dir={theme.direction}>
+              {tab1List.map(res=>(
+                  <div>{/* endDate value is onGoing when ongoing is checked*/}
+              <div style={{display:'none'}}>{res.hasOwnProperty('onGoing') && res.onGoing?res.endDate="On Going" :null}</div>
                     <ListItem style={{ display:'inline'}}>
                     <div style={{float:'right'}}> <EditButton id={res._id} toEdit={myEditCallback} toDelete={myDeleteCallback}/> </div>
-                      {Object.entries(res).map(([key,value],i) => (checkUnwanted(key,value) && <div> {fieldNames[key]} : {handleValue(value)} </div>))}
+                      {Object.entries(res).map(([key,value],i) => (checkUnwanted(key,value) && <div> {fieldNames[key]} : {handleDataValue(key,value,res)} </div>))}
                     </ListItem> 
                     {++count < tab1List.length? <Divider/>:null}
                     </div>
-                  ))}
-              </TabPanel>
-              <TabPanel value={value} index={1}>
-                  {tab2List.map(res=>(
-                  <div>
+                  ))} 
+            </TabPanel>
+            <TabPanel value={value} index={1} dir={theme.direction}>
+             {tab2List.map(res=>(
+                  <div> 
                     {/* endDate value is onGoing when ongoing is checked */}
                     <div style={{display:'none'}}>{res.hasOwnProperty('onGoing') && res.onGoing?res.endDate="On Going" :null}</div>
                     <ListItem style={{ display:'inline'}}>
                     <div style={{float:'right'}}> <EditButton id={res._id} toEdit={myEditCallback} toDelete={myDeleteCallback} /> </div>
-                      {Object.entries(res).map(([key,value],i) => (checkUnwanted(key) && <div> {fieldNames[key]} : {handleValue(key,value,res)} </div>)) }
+                      {Object.entries(res).map(([key,value],i) => (checkUnwanted(key) && <div> {fieldNames[key]} : {handleDataValue(key,value,res)} </div>)) }
                     </ListItem> 
                     {++count < tab2List.length? <Divider/>:null}
                     </div>
                   ))}
-              </TabPanel>
-            </div>            
-          </Typography>
+            </TabPanel>
+        </div>
         </DialogContent>
         <DialogActions>
           <Button autoFocus onClick={handleClose} color="primary">
