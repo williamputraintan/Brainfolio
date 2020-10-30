be86da927cd723912c9e7e3e7ab5162a01cdbe0f
franklinaldo <franklin.aldo@yahoo.com>
2020-10-31 10:40:38 +1100

    Rearrange project page structure

--- my-app/src/controllers/Project/FileController.jsx
+++ my-app/src/controllers/Project/FileController.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid'
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import IconButton from '@material-ui/core/IconButton';
-import GetAppIcon from '@material-ui/icons/GetApp';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 import { Container, Typography } from '@material-ui/core';
 import pdf from '../../images/icon/pdf-icon.png'
 
@@ -16,14 +18,19 @@ const useStyles = makeStyles((theme) => ({
     flexWrap: 'wrap',
     justifyContent: 'space-around',
     overflow: 'hidden',
+    maxWidth: 256,
+    height: 300
     // backgroundColor: theme.palette.background.paper,
   },
-  gridList: {
-    flexWrap: 'nowrap',
-    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-    transform: 'translateZ(0)',
-    cellHeight: '300px',
-    padding: theme.spacing(0,0,2,0)
+  // gridList: {
+  //   flexWrap: 'nowrap',
+  //   // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+  //   transform: 'translateZ(0)',
+  //   cellHeight: '300px',
+  //   padding: theme.spacing(0,0,2,)
+  // },
+  fileDesc: {
+    overflow: "scroll"
   },
   title: {
     color: 'white',
@@ -33,52 +40,59 @@ const useStyles = makeStyles((theme) => ({
       'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
   },
   file: {
-    padding: theme.spacing(0,2,2,0)
+    padding: theme.spacing(0,0,0),
+    maxHeight: "100"
   },
-  fileLabel:{
-    color: "white",
-    width:"100%",
-    padding: theme.spacing(2,2,2,0)
+  fileTitle:{
+    overflow: "hidden",
+    textOverflow: "..."
+    // color: "white",
+    // width:"100%",
+    // padding: theme.spacing(0,0,2)
   },
 }));
 
 export default function SingleLineGridList(projectDisplay) {
   const classes = useStyles();
   const files = projectDisplay.data;
+  // const files = [[],[],[],[],[],[]]
   console.log(files)
   // let files2 = [files]
   // console.log(files2)
 
   return (
-    <Container>
-      <Grid container >
-        <Grid item xs={12} >
-          <Typography variant="h4" className={classes.fileLabel}>
-            Files Preview
-          </Typography>
-        </Grid>
-      
-        <GridList className={classes.gridList} cellHeight='350'>
-          {files.map((file) => (
-            <GridListTile key={file}>
-              {/* {console.log(file[0].slice(-3))} */}
-              {(file[0].slice(-3))=="pdf" ? <img src={pdf}/>  : <img src={file[1]} />}
-              <GridListTileBar
-                title={file[0]}
-                classes={{
-                  root: classes.titleBar,
-                  title: classes.title,
-                }}
-                actionIcon={
-                  <IconButton aria-label={`star ${file[0]}`} href={file[1]}>
-                    <GetAppIcon className={classes.title} />
-                  </IconButton>
-                }
+
+    <Grid container>
+      <Typography variant="h4" className={classes.fileLabel}>
+        Files
+      </Typography>
+      <Grid container className={classes.file} spacing={4}>
+        {files.map((file) => (
+          <Grid item key={file} xs={12} sm={4} md={4}>
+            <Card className={classes.root}>
+              <CardMedia
+                component="img"
+                alt={file[0]}
+                height="216"
+                image={(file[0].slice(-3))=="pdf" ? pdf : file[1]}
+                title= {file[1]}
               />
-            </GridListTile>
-          ))}
-        </GridList>
+              <CardContent className={classes.fileDesc}>
+                <Typography className={classes.fileTitle} gutterBottom variant="body1" component="body1">
+                  {file[0]}
+                </Typography>
+              </CardContent>
+
+              <CardActions>
+                <Button size="small" color="primary">
+                  Download
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
       </Grid>
-    </Container>
+
+    </Grid>
   );
 }
--- my-app/src/controllers/ProjectController.jsx
+++ my-app/src/controllers/ProjectController.jsx
@@ -1,13 +1,13 @@
 import React, {useRef, useState, Suspense} from 'react'
 import {Grid ,Avatar, Button} from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
-import SectionMenu from "../components/Portfolio/MenuSection"; 
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { useTheme } from '@material-ui/core/styles';
 import { useLocation } from "react-router-dom";
 
 // Dark Mode imports
 import DarkTheme from "../utils/theme/DarkTheme";
+import MinimalTheme from "../utils/theme/MinimalTheme"
 import { makeStyles ,createMuiTheme, ThemeProvider} from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
@@ -39,39 +39,39 @@ const useStyles = makeStyles((theme) => ({
     }
   },
   author:{
-    backgroundColor: "#5584C2",
+    backgroundColor: "#3368B6",
     color: "white",
     width:"100%",
-    padding: theme.spacing(3),
+    padding: theme.spacing(3,0,3)
     // borderRadius: 3
   },
   description: {
     backgroundColor: "#3368B6",
     color: "white",
     width:"100%",
-    padding: theme.spacing(3),
+    padding: theme.spacing(1,4,3),
     // borderRadius: 3
   },
   title:{
     backgroundColor: "#174BA8",
-    color: "white",
     width:"100%",
-    padding: theme.spacing(3)     
+    padding: theme.spacing(3,4,3)
   },
   files:{
     backgroundColor: "#7AA6D2",
-    color: "white",
     width:"100%",
-    padding: theme.spacing(3),
+    padding: theme.spacing(3,4,3),
     // borderRadius: 3
   },
   video:{
     backgroundColor: "#95BEDD",
-    color: "white",
     width:"100%",
-    padding: theme.spacing(3),
+    padding: theme.spacing(3,0,3),
     // borderRadius: 3
   },
+  pad: {
+    padding: theme.spacing(3,4,3),
+  }
 }));
 
 function ProjectController() {
@@ -79,12 +79,8 @@ function ProjectController() {
   const classes = useStyles();
 
   const { pathname } = useLocation();
-console.log("Pathname "+ pathname);
+  console.log("Pathname "+ pathname);
   const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.up('sm'));
-  const isMobile = useMediaQuery(theme.breakpoints.down('sm'), {
-    defaultMatches: true
-  });
   const [project,setProject] = useState({
     contributor: [],
     projectFileName: [],
@@ -142,43 +138,57 @@ console.log("Pathname "+ pathname);
   console.log(project);
   // console.log(project.projectFileName)
   // Use ThemeProvider and CSSbaseline for darkmode
+  function showVideo(url){
+    if (url) {
+      return (
+        <Grid container item xs={12}
+         className={classes.video}>
+          <VideoController url={project.youtubeLink}/>
+        </Grid>
+      )
+    }
+  }
   return (
       <Suspense fallback={SkeletonCard}>
-        <div>
+        <div className={classes.root}>
+          <Paper>
+            <Container maxWidth="md" >
+              <Grid container >
+                <Grid container>
+                  <Grid item xs={12} sm={12}
+                    className={classes.title}
+                  >
+                    <Typography variant="h3" align="Left">
+                      {project.title}
+                    </Typography>
+                    <Typography variant="h5" align="Left"> 
+                      {project.startDate} - {project.endDate}
+                    </Typography>
+                  </Grid>
+                  
+                  <Grid container item xs={12} sm={12} md={12}
+                    className = {classes.description}
+                  >
+                    <DescriptionController data={project.description}/>   
+                  </Grid>
+                    
+                    {showVideo(project.youtubeLink)}
 
-          <Grid container spacing={3}>
-            <Paper item xs={12} sm={12}
-              className={classes.title}
-            >
-              <Typography variant="h3" align="center">
-                {project.title}
-              </Typography>
-              <Typography variant="h5" align="center"> 
-                {project.startDate} - {project.endDate}
-              </Typography>
-            </Paper>
-              <Grid container item xs={12} sm={12} md={8}
-                className = {classes.description}
-              >
-                <DescriptionController data={project.description}/>   
-              </Grid>
+                    <Grid container item xs={12} sm={12} md={12}
+                     className = {classes.author}
+                    >
+                      <AuthorController data={project.contributor}/>
+                    </Grid>
 
-              <Grid container item xs={12} sm={12} md={4}
-              className = {classes.author}
-              >
-                <AuthorController data={project.contributor}/>
+                    <Grid container item xs={12}
+                    className={classes.files}
+                    >
+                      <FileController data={project.projectFileName}/>
+                    </Grid>
+                </Grid>
               </Grid>
-
-              <Grid container item xs={12}
-              className={classes.files}
-              >
-                <FileController data={project.projectFileName}/>
-              </Grid>
-              <Grid container item xs={12}
-               className={classes.video}>
-                <VideoController url={project.youtubeLink}/>
-              </Grid>
-          </Grid>
+            </Container>
+        </Paper>
 
         </div>
         
