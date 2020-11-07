7d45d54e06820bf2b35c9fc6a4a29bb333ac2aae
franklinaldo <franklin.aldo@yahoo.com>
2020-11-07 18:47:58 +1100

    Adding 2 custom components

---
+++ my-app/src/components/Portfolio/Custom2ListItem/index.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Divider from '@material-ui/core/Divider';
+import { format, parseISO  } from "date-fns";
+
+const useStyles = makeStyles( theme => ({
+  root:{
+    paddingTop:theme.spacing(1),
+  },
+  title:{
+    fontWeight: 700,
+    fontSize: "1.333rem"
+  },
+  subTitle:{
+    fontWeight: 700,
+    fontSize: "1.2rem"
+  },
+  desc:{
+    fontWeight: 700,
+    fontSize: "1rem"
+  },
+  row: {
+    display: "flex",
+    // justifyContent:"space-between",
+  },
+  button: {
+    marginTop: theme.spacing(4),
+    cursor: "pointer",
+    display: "block"
+  }
+
+}));
+
+function ProjectListItem(props) {
+  const { data } = props;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      {/* <div className={classes.row}> */}
+        <Typography className={classes.title} variant="h2">
+            {data.itemTitle}
+        </Typography>
+        {/* <br/> */}
+        <Typography variant="subtitle1" gutterBottom>
+            {data.itemSubTitle}
+        </Typography>
+      {/* </div> */}
+
+      <Typography variant="body1">
+        {data.description}
+      </Typography>
+
+      {/* <Divider/> */}
+    </div>
+  )
+}
+
+const areEqual = (prevProps, nextProps) => { return prevProps.data === nextProps.data}
+const memoize = React.memo(ProjectListItem, areEqual)
+
+export default memoize;
---
+++ my-app/src/controllers/Porfolio/Custom2Controller.jsx
@@ -0,0 +1,122 @@
+import React, {useState, useContext} from 'react'
+import { StoreContext } from '../../context/store.context';
+import { makeStyles } from '@material-ui/core/styles';
+import CardAccent from "../../common/CardAccent";
+import Typography from '@material-ui/core/Typography';
+import Custom2ListItem from "../../components/Portfolio/Custom2ListItem"
+
+import AxiosInstance from "../../utils/axios";
+import axios from 'axios'
+import SkeletonCard from "../../common/SkeletonCard";
+
+const useStyles = makeStyles( theme => ({
+  root: {
+    padding: theme.spacing(2),
+    borderBottom: `5px solid ${theme.palette.primary.main}`
+  },
+  label:{
+    fontWeight: 700
+  },
+  title:{
+    paddingTop: theme.spacing(2),
+    fontWeight: 700,
+    fontSize: "1.333rem"
+  },
+  subTitle:{
+    fontWeight: 700,
+    fontSize: "1.2rem"
+  },
+  desc:{
+    fontSize: "1rem",
+    paddingTop: theme.spacing(1)
+  },
+
+}));
+
+const accentColor ="#1E88E5";
+
+function Custom2Controller(props) {
+  const classes = useStyles();
+  const {state} = useContext(StoreContext);
+  const config = {
+    headers: { Authorization: `Bearer ${state.token}` }
+  };
+
+  const { user } = props;
+
+
+  const [loading, setLoading] = useState(true);
+  const [custom, setCustom] = useState([]);
+  const [sectionTitle, setSectionTitle] = useState("");
+  React.useEffect(() => {
+    setLoading(true);
+    const source = axios.CancelToken.source();
+   
+
+    AxiosInstance
+      .get(`/public/custom/${user}`, config)
+      .then(response => {
+        // const { data } = response;
+        // setProjects(data);
+        const data = response?.data;
+        console.log("dataaaa", data)
+        if(data) {
+          setCustom(data.custom2.data);
+          setSectionTitle(data.custom2.sectionTitle);
+        }
+        
+      })
+    
+    setLoading(false);
+    return () => {
+      source.cancel(
+        "Canceled because of component unmounted"
+      );
+    };
+    },[user])
+
+    if ((custom.length==undefined) || (custom.length) < 1) {
+      return (
+        <>
+        </>
+      )
+    }
+
+  console.log("CUSTOM1:",custom);
+  console.log("SECTION ", sectionTitle);
+  return (
+    <CardAccent className={classes.root} color={accentColor}>
+      <Typography className={classes.label} variant="h4" gutterBottom>
+        {sectionTitle}
+      </Typography>
+      {
+        loading? <SkeletonCard/>:
+        custom.map((value,key) => {
+          return(
+            <Custom2ListItem key={key} data={value}/>
+          )
+        })
+      }
+    </CardAccent>
+    
+    // <CardAccent className={classes.root} color={accentColor}>
+    //   <Typography className={classes.label} variant="h4">
+    //     {sectionTitle}
+    //   </Typography>
+
+      
+    //    <Typography className={classes.title} variant="h3">
+    //         {"title"}
+    //     </Typography>
+    //     <Typography variant="subtitle1" gutterBottom>
+    //       {"subtitile"}
+    //     </Typography>
+
+    //   <Typography className={classes.desc} variant="body1">
+    //     {"Body"}
+    //   </Typography>
+
+    // </CardAccent>
+  )
+}
+export default Custom2Controller
